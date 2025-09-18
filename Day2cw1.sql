a60894bbf18425900d84132187af02d5ea049843
akashjv02 <akashjv02@gmail.com>
2025-09-18 22:21:58 +0530

    Replace customer entries with student data in Day2cw1.sql

--- Day2cw1.sql
+++ Day2cw1.sql
@@ -1,3 +1,5 @@
-INSERT INTO customers VALUES ('8','William','Tokyo','4006','Japan');
-INSERT INTO customers VALUES ('9','Sophia','Berlin','10115','Germany');
-INSERT INTO customers VALUES ('10','James','Sydney','2000','Australia'):
+INSERT INTO students(id, name, age, department, grade)
+VALUES ('1', 'Akash', '23', 'Ba', '100'),
+('2', 'freddy', '23', 'Bcom', '100'),
+('3', 'Avanthika', '23', 'Bsc', '100'),
+('4', 'Isco', '23', 'Ba', '100');
