29010487912d0ba5feb1e6f242e524aa446a7dec
akashjv02 <akashjv02@gmail.com>
2025-09-18 15:58:34 +0530

    Add Day2cw1.sql with initial customer data entries

---
+++ Day2cw1.sql
@@ -0,0 +1,3 @@
+INSERT INTO customers VALUES ('8','William','Tokyo','4006','Japan');
+INSERT INTO customers VALUES ('9','Sophia','Berlin','10115','Germany');
+INSERT INTO customers VALUES ('10','James','Sydney','2000','Australia');
