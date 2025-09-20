b9b9a815cf4fa8567f6c94d162f306db6526135a
akashjv02 <akashjv02@gmail.com>
2025-09-20 12:03:37 +0530

    Add Day3cw.sql with initial product data insertion and queries

---
+++ Day3cw.sql
@@ -0,0 +1,22 @@
+--Insert datas into the products_table--
+INSERT INTO products_table(id,name,category,price,in_stock)
+VALUES ('Laptop','Electronics',45000,'yes'),
+('Mouse','Electronics',450,'yes'),
+('Shirt','Cloths',2500,'No'),
+('Table','Furniture',25000,'No'),
+('Shoes','Cloths',400,'Yes');
+
+--Show all the unique products--
+SELECT DISTINCT category FROM products_table;
+
+--select all products that are in stock and have a price less than 500--
+SELECT * FROM products_table WHERE in_stock = 'yes' AND price < 500;
+
+--select all products that are not in stock or have a price greater than 1000--
+SELECT * FROM products_table WHERE in_stock = 'No' OR price > 1000;
+
+--show the names and prices of all products, and sort the results by price from highest to lowest--
+SELECT name, price FROM products_table ORDER BY price DESC;
+
+--Display the name and an expression showing the price with 18% tax added--
+SELECT name, price * 1.18 AS 'price_with_tax' FROM products_table;
