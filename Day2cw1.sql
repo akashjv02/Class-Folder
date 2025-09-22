3e17bde67fe7c9d58936267c4b7504a6cf328d06
akashjv02 <akashjv02@gmail.com>
2025-09-22 09:32:28 +0530

    Update student entries and fix comment typo in Day2cw1.sql

--- Day2cw1.sql
+++ Day2cw1.sql
@@ -1,9 +1,9 @@
 --Students Information--
 INSERT INTO students(id, name, age, department, grade)
 VALUES ('1', 'Akash', '23', 'Ba', '100'),
-('2', 'freddy', '23', 'Bcom', '100'),
-('3', 'Avanthika', '23', 'Bsc', '100'),
-('4', 'Isco', '23', 'Ba', '100');
+('2', 'freddy', '19', 'Computer Science', '75'),
+('3', 'Avanthika', '20', 'Physics', '90'),
+('4', 'Isco', '19', 'Ba', '88');
 
 --Display All Students whose age is greater than 20--
 SELECT * FROM students WHERE age > 20;
@@ -14,5 +14,5 @@ SELECT * FROM students WHERE department IN ('Computer Science', 'Physics');
 --Show the Student has the grade exactly equal to 90--
 SELECT * FROM students WHERE grade = 90;
 
---Find students whose gradebare between 70 and 90--
+--Find students whose grades are between 70 and 90.--
 SELECT * FROM students WHERE grade BETWEEN 70 AND 90;
